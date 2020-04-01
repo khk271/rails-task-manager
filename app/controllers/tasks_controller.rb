5eab4bd818c368b0ec9364082a4623f0f0c14ee6
Kevin Kim <khk271@nyu.edu>
2020-04-01 19:05:00 -0400

    Completed update and delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,44 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :delete]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id].to_i)
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def edit; end
+
+  def update
+    @task.update(task_params)
+
+    redirect_to tasks_path
+  end
+
+  def delete
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  get 'tasks/index', to: 'tasks#index'
+  get 'tasks/index', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: 'tasks#new', as: :new
   get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  post 'tasks/index', to: 'tasks#create'
+  patch 'tasks/:id', to: 'tasks#update', as: :update
+  delete 'tasks/:id', to: 'tasks#delete', as: :delete
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
